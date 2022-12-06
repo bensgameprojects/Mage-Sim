eb5035506945047b9f919a0d2fefae62aeb4eeab
bensgameprojects <lowe.benjamin@ymail.com>
2022-12-06 09:58:39 -0800

    Starting Simulation system
    Building the buildings so you can build buildings.

--- SceneSwitcher.gd
+++ SceneSwitcher.gd
@@ -3,6 +3,11 @@ extends Node
 # This Scene Switcher will transfer between the simulation
 # and the ARPG world parts
 # since each can be viewed as a separate game
+# The SceneSwitcher is the SOLE MEMBER of the group called SceneSwitcher which
+# can be used to find this node and connect with it like this:
+# var scene_switcher = get_tree().get_nodes_in_group("SceneSwitcher")[0]
+# then you can connect to it with connect(signal, scene_switcher, method_name)
+# from wherever you are
 
 var player_scene = preload("res://Player/Player.tscn")
 # will handle transferring the data of the player and any other necessary info
@@ -17,20 +22,9 @@ func _ready():
 	add_child(current_scene)
 	current_scene.add_player(player_scene.instance())
 
-#func change_scene(new_scene, old_scene):
-#	var inventory = old_scene.save_inventory()
-#	new_scene.load_inventory(inventory)
-#
-#
-#func _on_change_level(destinationScene):
-#	var new_scene = load(destinationScene).instance()
-#	new_scene.load_inventory(current_scene.save_inventory())
-	
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
-
-
+# this is called by a ZoneChanger node (see ZoneChanger.gd for details)
+# ZoneChanger emits a level_change signal with the path to the destination scene
+# This is resolved here.
 func _on_SceneSwitcher_change_level(destination_scene_path):
 	levelTransitionAnimation.play("fade_in")
 	# load an instance of the destination scene
---
+++ Simulation.gd
@@ -0,0 +1,10 @@
+extends Node
+
+# This node will keep track of the simulation
+# For the buildings that need to use it
+# This node is the only member of the Simulation group
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
